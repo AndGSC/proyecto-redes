81a680aae2a308ec30aa9eceff6ae9b3358a3289
paulinaporras <paulina.porras.nunez@est.una.ac.cr>
2026-05-10 18:00:56 -0600

    clean terraform files

--- Codigo_Terraform_Linux/RedesProject/main.tf
+++ Codigo_Terraform_Linux/RedesProject/main.tf
@@ -69,6 +69,41 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "LDAP"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "LDAPS"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "636"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 150
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # ---------------------------
@@ -76,8 +111,9 @@ resource "azurerm_network_security_group" "nsg" {
 # ---------------------------
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = azurerm_network_security_group.nsg.id  
 }
+
 
 # ---------------------------
 # PUBLIC IP + LOAD BALANCER
@@ -139,6 +175,7 @@ resource "azurerm_network_interface" "nic1" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.10.1.10"
+    public_ip_address_id = azurerm_public_ip.vm1_ip.id
   }
 }
 
@@ -152,6 +189,7 @@ resource "azurerm_network_interface" "nic2" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.10.1.11"
+    public_ip_address_id = azurerm_public_ip.vm2_ip.id
   }
 }
 
@@ -220,6 +258,77 @@ resource "azurerm_linux_virtual_machine" "vm2" {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+# IPs públicas para acceso SSH desde Ansible
+resource "azurerm_public_ip" "vm1_ip" {
+  name                = "vm1-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_public_ip" "vm2_ip" {
+  name                = "vm2-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Regla HTTPS en el Load Balancer
+resource "azurerm_lb_rule" "https" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "https-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "frontend"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend.id]
+  probe_id                       = azurerm_lb_probe.probe.id
+}
+
+# ---------------------------
+# NIC WINDOWS SERVER
+# ---------------------------
+resource "azurerm_network_interface" "nic_ad" {
+  name                = "nic-redesProject-ad"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.10.1.20"
+  }
+}
+
+# ---------------------------
+# VM WINDOWS SERVER 2016
+# ---------------------------
+resource "azurerm_windows_virtual_machine" "vm_ad" {
+  name                = "vm-ad-redesProject"
+  computer_name       = "vm-ad-redes"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B2s"
+  admin_username      = "adminRedes"
+  admin_password      = "ADRedes2026!"
+
+  network_interface_ids = [azurerm_network_interface.nic_ad.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
 }
--- Codigo_Terraform_Linux/RedesProject/outputs.tf
+++ Codigo_Terraform_Linux/RedesProject/outputs.tf
@@ -8,3 +8,11 @@ output "vm_private_ips" {
 output "lb_public_ip" {
   value = azurerm_public_ip.lb_ip.ip_address
 }
+
+output "vm1_public_ip" {
+  value = azurerm_public_ip.vm1_ip.ip_address
+}
+
+output "vm2_public_ip" {
+  value = azurerm_public_ip.vm2_ip.ip_address
+}
